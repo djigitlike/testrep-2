494b41d2f4974aaae6361a5bd1f9747a3b9d25ba
Andrii <andriip>
2023-10-31 18:19:02 +0200

    add script

---
+++ script.py
@@ -0,0 +1,14 @@
+import requests
+
+github_token = os.environ.get('GITHUB_TOKEN')
+
+url = 'https://api.github.com/repos/djigitlike/testrep-2/pulls?base=release'
+headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': 'Bearer {github_token}'
+}
+
+resp = requests.get(url=url, headers=headers)
+data = resp.json()
+
+exit(1)
