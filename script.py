b4dfed625c5fe2c7d71aecfb20695b13aa861c7c
Andrii <andriip>
2023-11-01 13:32:56 +0200

    Change script

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ github_token = os.environ.get('GITHUB_TOKEN')
 url = 'https://api.github.com/repos/djigitlike/testrep-2/pulls?base=release'
 headers = {
     'Accept': 'application/vnd.github+json',
-    'Authorization': 'Bearer {github_token}'
+    'Authorization': 'Bearer {}'.format(github_token)
 }
 
 resp = requests.get(url=url, headers=headers)
@@ -15,5 +15,5 @@ pull_requests = resp.json()
 pr_dev_to_release_list = [pr for pr in pull_requests if pr['head']['ref'] == 'dev']
 
 # pre_release has been created by someone
-if pr_dev_to_release_list.length > 1:
+if len(pr_dev_to_release_list) > 0:
     exit(1)
